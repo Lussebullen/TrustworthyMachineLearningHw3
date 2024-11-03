1757ba269eeb461c7965f19f5754e3b6ab18ab3b
Mathias Duedahl <duedahl@kth.se>
2024-11-03 15:40:44 +0600

    Pt2 started

--- Robust/robust_aggregator.py
+++ Robust/robust_aggregator.py
@@ -1,7 +1,17 @@
 # Put any functions you write for Question 1 here. 
+import numpy as np
 
 # For question 2, it would be helpful to define a function that returns the set of pruned gradients
 def robust_aggregator(gradients):
+    # Calculate covariance matrix
+    cov_matrix = np.cov(gradients)
+    # Calculate eigenvalues and eigenvectors
+    eig_values, eig_vectors = np.linalg.eig(cov_matrix)
+    # Sort eigenvalues and eigenvectors in order of decreasing eigenvalues
+    sorted_indices = np.argsort(eig_values)[::-1]
+    sorted_eig_values = eig_values[sorted_indices]
+    sorted_eig_vectors = eig_vectors[:, sorted_indices]
+    
     # Run pruning procedure
     pruned_gradients = gradients
     return pruned_gradients
